9aa754e060c7c46eea9d3f2eb4866e64fab03d08
KanwarIITRPR <2023csb1126@iitrpr.ac.in>
2025-03-06 19:54:43 +0530

    Added support for all Formats
    Extended the code to support S, SB, U and UJ Formats, though they still require a lot of changes - mentioned in the comments of the code

--- remake.cpp
+++ remake.cpp
@@ -42,28 +42,6 @@ vector<string> Tokenize() {
     return tokens = currTokens;
 }
 
-vector<string> ExtractMachineCode() {
-    switch (instructionFormatMapping[tokens[0]]) {
-        case R:
-            R_FormatDivision();
-            break;
-        case I:
-            I_FormatDivision();
-            break;
-        case S:
-            break;
-        case SB:
-            break;
-        case U:
-            break;
-        case UJ:
-            break;
-        default:
-            cerr << "Error: Unidentified instruction!";
-            break;
-    }
-    return machineCodeDivision;
-}
 
 void R_FormatDivision() {
     string opcode = r_opcode[tokens[0]];
@@ -88,13 +66,14 @@ void R_FormatDivision() {
     machineCode = funct7 + rs2 + rs1 + funct3 + rd + opcode;
 }
 
+// Convert offset(register) to valid arguments for load
 void I_FormatDivision() {
     string opcode = i_opcode[tokens[0]];
     string rd = tokens[1];
     string funct3 = i_func3[tokens[0]];
     string rs1 = tokens[2];
     string immediate = tokens[3];
-
+    
     rd.erase(0, 1);
     rs1.erase(0, 1);
     
@@ -107,6 +86,124 @@ void I_FormatDivision() {
     machineCodeDivision[6] = immediate;
     
     machineCode = immediate + rs1 + funct3 + rd + opcode;
+}
+
+// Convert offset(register) to valid arguments
+void S_FormatDivision() {
+    string opcode = s_opcode[tokens[0]];
+    string funct3 = s_func3[tokens[0]];
+    string rs1 = tokens[1];
+    string rs2 = tokens[2];
+    string immediate = tokens[3];
+    
+    rs1.erase(0, 1);
+    rs2.erase(0, 1);
+    
+    string lowerImmediate = immediate.substr(7, 5);
+    string upperImmediate = immediate.substr(0, 7);
+    
+    machineCodeDivision[0] = opcode;
+    machineCodeDivision[1] = funct3;
+    machineCodeDivision[2] = "NULL";
+    machineCodeDivision[3] = "NULL";
+    machineCodeDivision[4] = rs1;
+    machineCodeDivision[5] = rs2;
+    machineCodeDivision[6] = immediate;
+    
+    machineCode = upperImmediate + rs2 + rs1 + funct3 + lowerImmediate + opcode;
+}
+
+// Convert labels into relative addressing
+void SB_FormatDivision() {
+    string opcode = sb_opcode[tokens[0]];
+    string funct3 = sb_func3[tokens[0]];
+    string rs1 = tokens[1];
+    string rs2 = tokens[2];
+    string immediate = tokens[3];
+    
+    rs1.erase(0, 1);
+    rs2.erase(0, 1);
+    
+    string lowerImmediate = immediate.substr(8, 4) + immediate[1];
+    string upperImmediate = immediate[0] + immediate.substr(2, 6);
+    
+    machineCodeDivision[0] = opcode;
+    machineCodeDivision[1] = funct3;
+    machineCodeDivision[2] = "NULL";
+    machineCodeDivision[3] = "NULL";
+    machineCodeDivision[4] = rs1;
+    machineCodeDivision[5] = rs2;
+    machineCodeDivision[6] = immediate;
+    
+    machineCode = upperImmediate + rs2 + rs1 + funct3 + lowerImmediate  + opcode;
+}
+
+// Check for bounds
+void U_FormatDivision() {
+    string opcode = u_opcode[tokens[0]];
+    string rd = tokens[1];
+    string immediate = tokens[2];
+    
+    rd.erase(0, 1);
+    
+    machineCodeDivision[0] = opcode;
+    machineCodeDivision[1] = "NULL";
+    machineCodeDivision[2] = "NULL";
+    machineCodeDivision[3] = rd;
+    machineCodeDivision[4] = "NULL";
+    machineCodeDivision[5] = "NULL";
+    machineCodeDivision[6] = immediate;
+    
+    machineCode = immediate + rd + opcode;
+}
+
+void UJ_FormatDivision() {
+    string opcode = uj_opcode[tokens[0]];
+    string rd = tokens[1];
+    string immediate = tokens[2];
+    
+    rd.erase(0, 1);
+
+    string adjustedImmediate = immediate[0] + immediate.substr(10, 10) + immediate[9] + immediate.substr(1, 8);
+    
+    machineCodeDivision[0] = opcode;
+    machineCodeDivision[1] = "NULL";
+    machineCodeDivision[2] = "NULL";
+    machineCodeDivision[3] = rd;
+    machineCodeDivision[4] = "NULL";
+    machineCodeDivision[5] = "NULL";
+    machineCodeDivision[6] = immediate;
+    
+    machineCode = adjustedImmediate + rd + opcode;
+}
+
+// Check for register bounds
+// Convert the registers and immediate values to hexadecimal format
+vector<string> ExtractMachineCode() {
+    switch (instructionFormatMapping[tokens[0]]) {
+        case R:
+            R_FormatDivision();
+            break;
+        case I:
+            I_FormatDivision();
+            break;
+        case S:
+            S_FormatDivision();
+            break;
+        case SB:
+            SB_FormatDivision();
+            break;
+        case U:
+            U_FormatDivision();
+            break;
+        case UJ:
+            UJ_FormatDivision();
+            break;
+        default:
+            cerr << "Error: Unidentified instruction!";
+            break;
+    }
+    return machineCodeDivision;
 }
 
 void InitializeInstructions() {
